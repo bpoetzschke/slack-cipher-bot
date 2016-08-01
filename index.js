2590b99f54634ccf0677ce93d1ad520e9dc730b3
Bjoern Poetzschke <bpoetzschke@newstore.com>
2016-08-01 23:41:09 +0200

    added basic functionality

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var Botkit = require('botkit'),
+    controller = Botkit.slackbot({
+        logLevel: 3
+    });
+
+controller.spawn({
+    token: process.env.SLACK_TOKEN
+}).startRTM(function (err) {
+    if (err) {
+       throw new Error('Could not connect to slack');
+    }
+    console.log('Connected to slack');
+});
+
+
+// Listen to on join channel event and log
+controller.on('bot_channel_join', function() {
+    console.log('Joined channel');
+});
+
+//Listen to all messages from a channel
+controller.on('direct_mention', function(bot, message) {
+    if (message.type === 'message') {
+        bot.reply(message, {
+            text: "Aye! I ciphered the following for you:",
+            attachments: [{
+                    text: translate(message.text)
+                }]
+            }
+        );
+    }
+});
+
+function translate(text) {
+    return text.toUpperCase();
+}
