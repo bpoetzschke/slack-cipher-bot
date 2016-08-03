4e79a97044b31058a8e515567eab3c53718878cb
Bjoern Poetzschke <bpoetzschke@newstore.com>
2016-08-03 13:47:45 +0200

    updated translator
    Also translates now back to original language

--- translator.js
+++ translator.js
@@ -1,12 +1,20 @@
 "use strict";
 
 var rp = require('request-promise'),
-    languages = ['ar', 'bn', 'cy', 'is', 'en'];
+    languages = ['yi', 'pa', 'cy', 'is', 'tg', 'gd', ''];
 function translate(message, callback) {
-    doRequest("ar", message, callback);
+    startRequest(message, callback);
 }
 
-function doRequest(language, text, callback) {
+function startRequest(text, callback) {
+    doRequest(languages[0], 0, text, "", callback);
+}
+
+function doRequest(language, index, text, originLang, callback) {
+    if (index == languages.length - 1) {
+        language += originLang;
+    }
+
     var translateApiKey = process.env.TRANSLATE_API_KEY;
     var options = {
         uri: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + translateApiKey + '&text=' + encodeURIComponent(text) + '&lang=' + language,
@@ -16,12 +24,19 @@ function doRequest(language, text, callback) {
         json: true
     };
 
-    console.log('Sending translate request.');
-
     rp(options).then(function (data) {
         var translated = data.text[0];
 
-        callback(translated);
+        if (index == 0) {
+            originLang = data.lang.split('-')[0];
+        }
+
+        if (index == languages.length - 1) {
+            callback(translated);
+        } else {
+            var langCode = languages[index] + "-" + languages[index+1];
+            doRequest(langCode, index+1, translated, originLang, callback);
+        }
     });
 }
 
